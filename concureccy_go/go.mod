a84757d5f912eb806bce9bc28eb8506d53a4d62f
vladimir <profmail@yandex.ru>
2025-04-17 13:48:06 +0300

    concurency gorutime

---
+++ concureccy_go/go.mod
@@ -0,0 +1,3 @@
+module concurr
+
+go 1.24.2
--- concureccy_go/main.go
+++ concureccy_go/main.go
@@ -1,1 +1,72 @@
-package concureccy_go
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	//создать конвейер
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	//Запустить 3 рабочии горутины
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	for w := 1; w <= 3; w++ {
+		go func(id int) {
+			defer wg.Done()
+			processJobs(ctx, id, jobs, results)
+		}(w)
+	}
+
+	//загружаем jobs и закрываем
+	go func() {
+		for j := 1; j <= 9; j++ {
+			jobs <- j
+		}
+		close(jobs)
+	}()
+
+	// запускаем сборщик
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	//Обработать результаты
+	totalResult := 0
+	for r := range results {
+		totalResult += r
+		fmt.Printf("Получил результат: %d\n", r)
+	}
+	fmt.Printf("Окончательный результат: %d\n", totalResult)
+
+}
+
+func processJobs(ctx context.Context, id int, jobs <-chan int, results chan<- int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case job, ok := <-jobs:
+			if !ok {
+				return // канал закрыт
+			}
+			fmt.Printf("Worker %d processing job %d\n", id, job)
+			time.Sleep(100 * time.Millisecond) // имитация работы
+			select {
+			case results <- job * 2:
+				// Результат отправлен
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+}
