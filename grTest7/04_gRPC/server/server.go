29f8f62b6a8e311d8a09f0084220d433232639dd
vladimir <profmail@yandex.ru>
2025-08-26 09:16:14 +0300

    fix proto file

---
+++ grTest7/04_gRPC/server/server.go
@@ -0,0 +1,12 @@
+package server
+
+//import (
+//	pb "github.com/lvg-erp/sandbox/grTest7/05_grpc_exchange/proto"
+//	"sync"
+//)
+//
+//type messageExchangeServer struct {
+//	pb.UnimplementedMessageExchangeServiceServer
+//	mu                sync.Mutex
+//	processedMessages []string
+//}
